8c42df7546a3927487e74f697e94268084153331
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2024-02-28 16:48:25 +0200

    Fixes and refactoring

--- packages/examples/cvat/exchange-oracle/src/endpoints/serializers.py
+++ packages/examples/cvat/exchange-oracle/src/endpoints/serializers.py
@@ -24,6 +24,7 @@ def serialize_job(
 
         if isinstance(project, str):
             project = cvat_service.get_project_by_id(session, project)
+            assert project
         elif not isinstance(project, cvat_service.Project):
             assert False
 
@@ -36,6 +37,7 @@ def serialize_job(
         return service_api.JobResponse(
             id=project.id,
             escrow_address=project.escrow_address,
+            chain_id=project.chain_id,
             title=f"Job {project.escrow_address[:10]}",
             description=manifest.annotation.description if manifest else None,
             bounty=str(manifest.job_bounty) if manifest else None,
@@ -48,7 +50,7 @@ def serialize_job(
 def serialize_assignment(
     assignment: Union[str, cvat_service.Assignment],
     *,
-    project: Union[str, cvat_service.Project],
+    project: Union[None, str, cvat_service.Project] = None,
     manifest: Union[None, TaskManifest, Literal[False]] = None,
     session: Optional[Session] = None,
 ) -> service_api.AssignmentResponse:
@@ -61,8 +63,11 @@ def serialize_assignment(
         elif not isinstance(assignment, cvat_service.Assignment):
             assert False
 
-        if isinstance(project, str):
+        if project is None:
+            project = assignment.job.project
+        elif isinstance(project, str):
             project = cvat_service.get_project_by_id(session, project)
+            assert project
         elif not isinstance(project, cvat_service.Project):
             assert False
 
@@ -74,7 +79,9 @@ def serialize_assignment(
 
         return service_api.AssignmentResponse(
             id=assignment.id,
-            escrow_address=assignment.user_wallet_address,
+            escrow_address=project.escrow_address,
+            chain_id=project.chain_id,
+            wallet_address=assignment.user_wallet_address,
             size=manifest.annotation.job_size + manifest.validation.val_size
             if manifest
             else None,  # TODO: can be different for different types
@@ -88,6 +95,6 @@ def serialize_assignment(
             if not assignment.is_finished
             else None,
             started_at=assignment.created_at,
-            finishes_at=assignment.expires_at,
+            expires_at=assignment.expires_at,
             finished_at=assignment.completed_at,
         )
